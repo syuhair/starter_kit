b375dce24f81a0f85936ae2f8d0c1084a64f1f35
Syuhair Wardi <syuhair@gmail.com>
2020-10-25 01:37:04 +0800

    Header - Remove link to Dapp University Bootcamp

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,7 @@ class App extends Component {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
             ETH Swap 2.0
-          </a>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
