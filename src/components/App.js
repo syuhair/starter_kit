16e36a92e59898a0b9646db7142f88f97ded0cf6
Syuhair Wardi <syuhair@gmail.com>
2020-10-25 02:14:54 +0800

    Update Main Page

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,19 @@ class App extends Component {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-            ETH Swap 2.0
+          <div className="navbar-brand col-sm-3 col-md-2 mr-0">
+            Roti Canai Exchange
+          </div>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>ETH Swap 2.0</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
+                <img
+                  src={logo}
+                  className="App-logo"
+                  alt="Credit: Adib Mazlee"
+                />
               </div>
             </main>
           </div>
