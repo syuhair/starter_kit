9ffb04829999dde7d5af2ee42e2b920a40ea19ec
Syuhair Wardi <syuhair@gmail.com>
2020-10-27 22:34:33 +0800

    Deploy Smart Contract

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <div className="navbar-brand col-sm-3 col-md-2 mr-0">
-            Roti Canai Exchange
+            ETH Swap 2.0
           </div>
         </nav>
         <div className="container-fluid mt-5">
---
+++ src/contracts/EthSwap.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+contract EthSwap {
+    string public name = "Eth Swap 2.0";
+}
